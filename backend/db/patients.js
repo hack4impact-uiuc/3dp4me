525f37f2549351de524729775237c46fbc54656b
laurenho025 <laurenh4@illinois.edu>
2020-10-07 20:14:38 -0500

    Get request for patients by stage

--- backend/db/patients.js
+++ backend/db/patients.js
@@ -1,10 +1,19 @@
 const db = require("../pool.js");
 
 const getAllPatients = async () => {
-    let res = await db.query('SELECT * FROM patients');
+    const sql = "SELECT * FROM patients";
+    let res = await db.query(sql);
+    return res.rows;
+}
+
+const getPatientsByStage = async (stage) => {
+    const sql = "SELECT * FROM patients WHERE current_stage = $1";
+    const params = [ stage ];
+    let res = await db.query(sql, params);
     return res.rows;
 }
 
 module.exports = {
     getAllPatients,
+    getPatientsByStage,
 }
--- backend/routes/api/patients.js
+++ backend/routes/api/patients.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { errorWrap } = require("../../utils");
 
-const { getAllPatients } = require("../../db/patients.js");
+const { getAllPatients, getPatientsByStage } = require("../../db/patients.js");
 
 // Get all patients
 router.get(
@@ -13,7 +13,22 @@ router.get(
             code: 200,
             success: true,
             result: patients,
-        })
+        });
+    }),
+);
+
+// Get all patients in a stage
+router.get(
+    "/:stage",
+    errorWrap(async (req, res) => {
+        const { stage } = req.params;
+        
+        const patients = await getPatientsByStage(stage);
+        res.status(200).json({
+            code: 200,
+            success: true,
+            result: patients,
+        });
     }),
 );
 
