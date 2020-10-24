aed5ff19ffc4663056c9d22f635759c05f34528a
Lauren Ho <42976373+laurenho025@users.noreply.github.com>
2020-10-24 14:45:20 -0500

    Backend Setup for Mongo (#10)
    * Remove postgresql stuff from backend directory
    * Fix patient model and add model index.js

--- backend/routes/api/patients.js
+++ backend/routes/api/patients.js
@@ -1,49 +1,40 @@
 const express = require("express");
 const router = express.Router();
 const { errorWrap } = require("../../utils");
-
-const { getAllPatients, getPatientsByStage, completeStage } = require("../../db/patients.js");
+const { models } = require("../../models");
 
 // TODO: Get all patients
 router.get(
     '/',
     errorWrap(async (req, res) => {
-        const patients = await getAllPatients();
-        res.status(200).json({
-            code: 200,
-            success: true,
-            result: patients,
-        });
+        // res.status(200).json({
+        //     code: 200,
+        //     success: true,
+        //     result: patients,
+        // });
     }),
 );
 
-// Get all patients in a stage
+// TODO: Get all patients in a stage
 router.get(
     "/:stage",
     errorWrap(async (req, res) => {
-        const { stage } = req.params;
-        
-        const patients = await getPatientsByStage(stage);
-        res.status(200).json({
-            code: 200,
-            success: true,
-            result: patients,
-        });
+        // res.status(200).json({
+        //     code: 200,
+        //     success: true,
+        //     result: patients,
+        // });
     }),
 );
 
-// Mark stage as complete for a patient
+// TODO: Mark stage as complete for a patient
 router.post(
     "/:id/:stage/complete",
     errorWrap(async (req, res) => {
-        const { id, stage } = req.params;
-        const { userId } = req.body;
-
-        await completeStage(userId, id, stage);
-        res.status(200).json({
-            code: 200,
-            success: true,
-        });
+        // res.status(200).json({
+        //     code: 200,
+        //     success: true,
+        // });
     }),
 );
 
