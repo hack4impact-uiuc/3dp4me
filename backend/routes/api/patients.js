cc9c513e67146078e12a49911b6c4e2aa1433193
laurenho025 <laurenh4@illinois.edu>
2020-10-24 02:16:59 -0500

    Example GET request

--- backend/routes/api/patients.js
+++ backend/routes/api/patients.js
@@ -2,6 +2,16 @@ const express = require("express");
 const router = express.Router();
 const { errorWrap } = require("../../utils");
 
-// Endpoints here
+router.get(
+    '/',
+    errorWrap(async (req, res) => {
+        console.log("Hello World!");
+        res.status(200).json({
+            code: 200,
+            success: true,
+            result: "hi",
+        });
+    }),
+);
 
 module.exports = router;
