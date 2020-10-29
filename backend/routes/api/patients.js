b229e40283b98cde16e8ff75182b4e155ab225ab
laurenho025 <laurenh4@illinois.edu>
2020-10-28 19:49:41 -0500

    Initial work on edit status endpoint

--- backend/routes/api/patients.js
+++ backend/routes/api/patients.js
@@ -25,4 +25,32 @@ router.get(
     }),
 );
 
+// Edit a patient's status
+// TODO: Upload new files to AWS and update files field in model
+router.put(
+    '/:serial/:stage/status',
+    errorWrap(async (req, res) => {
+        const { status, userID, notes } = req.body;
+        const { serial, stage } = req.params;
+        const updatedPatient = await models.Patient.findOneAndUpdate(
+            {
+                "serial": serial
+            },
+            {
+                [stage]: {
+                    "status": status,
+                    "lastEdit": Date.now(),
+                    "lastEditBy": userID,
+                    "notes": notes
+                }
+            }
+        );
+        res.status(200).json({
+            code: 200,
+            success: true,
+            result: updatedPatient,
+        });
+    }),
+);
+
 module.exports = router;
