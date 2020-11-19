e3703e344628575b3abaed3c321c14af742a8241
laurenho025 <laurenh4@illinois.edu>
2020-11-18 18:53:18 -0600

    Line spacing thing

--- frontend/src/components/ToggleButtons/ToggleButtons.js
+++ frontend/src/components/ToggleButtons/ToggleButtons.js
@@ -40,63 +40,49 @@ const ToggleButtons = (props) => {
             <ToggleButtonGroup className="toggle-button-group" size="large" exclusive value={props.step} onChange={props.handleStep}>
                 <ToggleButton
                     disableRipple
-                    className={`toggle-button
-                        ${props.step === "info" ? "active" : ""}`
-                    }
+                    className={`toggle-button ${props.step === "info" ? "active" : ""}`}
                     value="info"
                 >
                     {props.medStatus !== undefined ? statusIcons[props.medStatus] : null} <b>{lang[key].components.stepTabs.patientInfo}</b>
                 </ToggleButton>
                 <ToggleButton
                     disableRipple
-                    className={`toggle-button
-                        ${props.step === "scan" ? "active" : ""}`
-                    }
+                    className={`toggle-button ${props.step === "scan" ? "active" : ""}`}
                     value="scan"
                 >
                     {props.earScanStatus !== undefined ? statusIcons[props.earScanStatus] : null} <b>{lang[key].components.stepTabs.earScan}</b>
                 </ToggleButton>
                 <ToggleButton
                     disableRipple
-                    className={`toggle-button
-                        ${props.step === "cad" ? "active" : ""}`
-                    }
+                    className={`toggle-button ${props.step === "cad" ? "active" : ""}`}
                     value="cad"
                 >
                     {props.modelStatus !== undefined ? statusIcons[props.modelStatus] : null} <b>{lang[key].components.stepTabs.CADModeling}</b>
                 </ToggleButton>
                 <ToggleButton
                     disableRipple
-                    className={`toggle-button
-                        ${props.step === "printing" ? "active" : ""}`
-                    }
+                    className={`toggle-button ${props.step === "printing" ? "active" : ""}`}
                     value="printing"
                 >
                     {props.printStatus !== undefined ? statusIcons[props.printStatus] : null} <b>{lang[key].components.stepTabs.print}</b>
                 </ToggleButton>
                 <ToggleButton
                     disableRipple
-                    className={`toggle-button
-                        ${props.step === "processing" ? "active" : ""}`
-                    }
+                    className={`toggle-button ${props.step === "processing" ? "active" : ""}`}
                     value="processing"
                 >
                     {props.processingStatus !== undefined ? statusIcons[props.processingStatus] : null} <b>{lang[key].components.stepTabs.processing}</b>
                 </ToggleButton>
                 <ToggleButton
                     disableRipple
-                    className={`toggle-button
-                        ${props.step === "delivery" ? "active" : ""}`
-                    }
+                    className={`toggle-button ${props.step === "delivery" ? "active" : ""}`}
                     value="delivery"
                 >
                     {props.deliveryStatus !== undefined ? statusIcons[props.deliveryStatus] : null} <b>{lang[key].components.stepTabs.delivery}</b>
                 </ToggleButton>
                 <ToggleButton
                     disableRipple
-                    className={`toggle-button
-                        ${props.step === "feedback" ? "active" : ""}`
-                    }
+                    className={`toggle-button ${props.step === "feedback" ? "active" : ""}`}
                     value="feedback"
                 >
                     {props.feedbackStatus !== undefined ? statusIcons[props.feedbackStatus] : null} <b>{lang[key].components.stepTabs.feedback}</b>
