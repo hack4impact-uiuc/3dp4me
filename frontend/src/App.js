b5859ddc5192b92a027f74c5385bc6f21d9748fc
Lauren Ho <42976373+laurenho025@users.noreply.github.com>
2020-12-09 22:08:57 -0600

    Basic account dropdown (#40)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import { withAuthenticator } from '@aws-amplify/ui-react';
 import { Amplify } from 'aws-amplify';
 import awsconfig from './aws/aws-exports.js';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
@@ -34,8 +34,8 @@ function App() {
   return (
     <body dir={key === "AR" ? "rtl": "ltr"}>
       <Router>
-        <Navbar lang={langInfo} />
-        <div className={`${key === "AR" ? "flip" : ""}`}>
+        <Navbar lang={langInfo} setLang={setKey} />
+        <div className={`${key == "AR" ? "flip" : ""}`}>
           <Switch>
             <div className="content">
               {/* Path = BASE_URL */}
@@ -60,9 +60,6 @@ function App() {
               </Route>
             </div>
           </Switch>
-        </div>
-        <div style={{width: '50px'}}>
-          <AmplifySignOut/>
         </div>
       </Router>
     </body>
