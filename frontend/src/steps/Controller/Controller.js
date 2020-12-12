c74a89ffd1888e499c0c06bcdd22d83097a14384
laurenho025 <laurenh4@illinois.edu>
2020-12-12 16:03:24 -0600

    Move manage patient modal to component and add feedback radio button

--- frontend/src/steps/Controller/Controller.js
+++ frontend/src/steps/Controller/Controller.js
@@ -14,10 +14,9 @@ import { Accordion, AccordionDetails, AccordionSummary, Button, TextField } from
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import patientFile from '../../Test Data/patient.json'
 import ToggleButtons from '../../components/ToggleButtons/ToggleButtons';
+import ManagePatientModal from '../../components/ManagePatientModal/ManagePatientModal';
 import reactSwal from '@sweetalert/with-react';
 import swal from 'sweetalert';
-
-import CloseIcon from '@material-ui/icons/Close';
 
 const theme = createMuiTheme({
     direction: 'rtl',
@@ -38,85 +37,12 @@ const Controller = (props) => {
     const lang = props.lang.data;
     const key = props.lang.key;
 
-    const handleArchivePatient = e => {
-        // TODO: Archive patient handling
-    }
-
-    const handleManagePatientSave = () => {
-        let name = document.getElementById("manage-patient-name").value;
-        let dob = document.getElementById("manage-patient-dob").value;
-        // TODO: call edit patient endpoint with new name / dob / archive selection
-        swal(lang[key].components.swal.managePatient.successMsg, "", "success");
-    }
-
-    const handleDeletePatient = () => {
-        swal({
-            title: lang[key].components.swal.managePatient.confirmDeleteMsg,
-            icon: "warning",
-            buttons: true,
-            dangerMode: true
-        }).then(willDelete => {
-            if (willDelete) {
-                swal({
-                    title: lang[key].components.swal.managePatient.deleteSuccessMsg,
-                    icon: "success"
-                }).then(() => {
-                    // TODO: call delete patient endpoint
-                    window.location.href = "/";
-                });
-            }
-        });
-    }
-
-    const handleManagePatientClose = () => {
-        swal.close();
-    }
-
     const managePatient = () => {
         reactSwal({
             className: 'controller-manage-patient-swal',
             buttons: {},
             content: (
-                <div className={`controller-manage-patient-wrapper ${key === "AR" ? "controller-manage-patient-wrapper-ar" : ""}`}>
-                    {key === "AR" ? 
-                        <div className="manage-patient-header">
-                            <Button onClick={handleManagePatientClose}><CloseIcon /></Button>
-                            <h2>{lang[key].components.swal.managePatient.title}</h2>
-                        </div> :
-                        <div className="manage-patient-header">
-                            <h2>{lang[key].components.swal.managePatient.title}</h2>
-                            <Button onClick={handleManagePatientClose}><CloseIcon /></Button>
-                        </div>
-                    }
-                    <div className="profile-information-wrapper">
-                        <h3>{lang[key].components.swal.managePatient.profileInformation}</h3>
-                        <p>{lang[key].components.swal.managePatient.name}</p>
-                        <TextField id="manage-patient-name" defaultValue={patientFile.patientInfo.name} />
-                        <p>{lang[key].components.swal.managePatient.dob}</p>
-                        <TextField id="manage-patient-dob" defaultValue={patientFile.patientInfo.dob} />
-                    </div>
-                    <div className="profile-management-wrapper">
-                        <h3>{lang[key].components.swal.managePatient.profileManagement}</h3>
-                        <p>{lang[key].components.swal.managePatient.archiveInformation}</p>
-                        {key === "AR" ? 
-                            <div className="profile-management-radio-button-group" onChange={handleArchivePatient}>
-                                <div>{lang[key].components.swal.managePatient.active} <input type="radio" value="active" name="archived" defaultChecked /></div>
-                                <div>{lang[key].components.swal.managePatient.archive} <input type="radio" value="archived" name="archived" /></div>
-                            </div> :
-                            <div className="profile-management-radio-button-group" onChange={handleArchivePatient}>
-                                <div><input type="radio" value="active" name="archived" defaultChecked /> {lang[key].components.swal.managePatient.active}</div>
-                                <div><input type="radio" value="archived" name="archived" /> {lang[key].components.swal.managePatient.archive}</div>
-                            </div>
-                        }
-                    </div>
-                    <div className={`manage-patient-delete ${key === "AR" ? "manage-patient-delete-ar" : ""}`}>
-                        <p>{lang[key].components.swal.managePatient.deleteInformation}</p>
-                        <Button className="manage-patient-delete-button" onClick={handleDeletePatient}>{lang[key].components.swal.managePatient.buttons.delete}</Button>
-                    </div>
-                    <div className="manage-patient-footer">
-                        <Button className="manage-patient-save-button" onClick={handleManagePatientSave}>{lang[key].components.swal.managePatient.buttons.save}</Button>
-                    </div>
-                </div>
+                <ManagePatientModal lang={props.lang} />
             )
         });
     }
