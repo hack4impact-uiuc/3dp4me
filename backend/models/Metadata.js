da7c5f6fafa9fe3f25040d4dcc90998a8e5a0cc2
gwang0136 <genew2@illinois.edu>
2021-02-11 19:58:02 -0800

    change field type to enum

--- backend/models/Metadata.js
+++ backend/models/Metadata.js
@@ -1,15 +1,28 @@
 const mongoose = require('mongoose');
 
+const fieldEnum = {
+    STRING: 'String',
+    FILE: 'File',
+    NUMBER: 'Number',
+    DATE: 'Date',
+    ENUM: 'Enum',
+};
+
 const stepSchema = new mongoose.Schema({
     key: { type: String, required: true },
     displayNameEnglish: { type: String, required: true },
     displayNameArabic: { type: String, required: true },
     stepNumber: { type: Number, required: true },
-    fields: { type: { fieldSchema }, required: true, default: [] },
+    fields: { type: [fieldSchema], required: true, default: [] },
 });
 
 const fieldSchema = new mongoose.Schema({
-    fieldType: { type: String, required: true },
+    fieldType: {
+        type: String,
+        enum: Object.values(fieldEnum),
+        required: true,
+        default: fieldEnum.STRING,
+    },
     displayNameEnglish: { type: String, required: true },
     displayNameArabic: { type: String, required: true },
 });
