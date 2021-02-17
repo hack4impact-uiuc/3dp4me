b2ae2b92cc0fc90b67d2e363b8927b649b5dd9a7
Daniel <dm32@illinois.edu>
2021-02-16 19:05:39 -0600

    Add validation for fields and keys for items

--- backend/models/Metadata.js
+++ backend/models/Metadata.js
@@ -8,32 +8,57 @@ const fieldEnum = {
     ENUM: 'Enum',
 };
 
-const stepSchema = new mongoose.Schema({
-    key: { type: String, required: true },
-    displayNameEnglish: { type: String, required: true },
-    displayNameArabic: { type: String, required: true },
-    stepNumber: { type: Number, required: true },
-    fields: { type: [fieldSchema], required: true, default: [] },
-});
-
 const fieldSchema = new mongoose.Schema({
+    fieldNumber: { type: Number, required: true },
+    key: { type: String, required: true },
     fieldType: {
         type: String,
         enum: Object.values(fieldEnum),
         required: true,
         default: fieldEnum.STRING,
     },
-    displayNameEnglish: { type: String, required: true },
-    displayNameArabic: { type: String, required: true },
-});
-
-const metaDataSchema = new mongoose.Schema({
-    steps: {
-        type: [stepSchema],
-        default: [],
+    isVisibleOnDashboard: { type: Boolean, required: true },
+    displayName: {
+        EN: { type: String, required: true },
+        AR: { type: String, required: true },
     },
+    readableGroups: { type: [String], required: true },
+    writableGroups: { type: [String], required: true },
 });
 
-const metaData = mongoose.model('metadata', metaDataSchema);
+const validateStep = async (fieldSchema) => {
+    var fieldNumbers = Object.create(null);
+    var fieldKeys = Object.create(null);
+    for (var i = 0; i < fieldSchema.length; ++i) {
+        var value = fieldSchema[i];
+        if (value.fieldNumber in fieldNumbers || value.key in fieldKeys) {
+            return false;
+        }
+        fieldNumbers[value.fieldNumber] = true;
+        fieldKeys[value.key] = true;
+    }
+    return true;
+};
 
-module.exports = { metaData };
+const stepSchema = new mongoose.Schema({
+    key: { type: String, required: true, unique: true },
+    displayName: {
+        EN: { type: String, required: true },
+        AR: { type: String, required: true },
+    },
+    stepNumber: { type: Number, required: true, unique: true },
+    fields: {
+        type: [fieldSchema],
+        required: true,
+        default: [],
+        validate: {
+            validator: validateStep,
+            message: 'fieldKeys and fieldNumbers must be unique',
+        },
+    },
+    readableGroups: { type: [String], required: true },
+    writableGroups: { type: [String], required: true },
+});
+
+const Step = mongoose.model('steps', stepSchema);
+module.exports = { Step };
