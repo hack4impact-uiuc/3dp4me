6d80ad804b9024650f7d24663b5eea0496edee08
riyajain59 <riyaj3@illinois.edu>
2021-02-23 18:44:49 -0600

    Dynamically generate menu items and labels

--- frontend/src/components/ToggleButtons/ToggleButtons.js
+++ frontend/src/components/ToggleButtons/ToggleButtons.js
@@ -13,6 +13,7 @@ import HalfCircleIcon from '../../assets/half-circle.svg';
 import { LanguageDataType } from '../../utils/custom-proptypes';
 import './ToggleButtons.scss';
 import { getStageMetadata } from '../../utils/api';
+import { keyBy, set } from 'lodash';
 
 const ToggleButtons = ({
     languageData,
@@ -72,7 +73,7 @@ const ToggleButtons = ({
         if (step != null) {
             setSteps(step);
         }
-    });
+    }, [setSteps]);
 
     function generateToggleButtons() {
         console.log(steps);
@@ -92,6 +93,36 @@ const ToggleButtons = ({
         });
     }
 
+    function generateLabels() {
+        return steps.map((element) => {
+            return (
+                <div className="toggle-button-selector">
+                    {step === element.key ? (
+                        <div className="current-step-label">
+                            {/*{medStatus !== undefined
+                                ? statusIcons[medStatus]
+                            : null}*/}
+                            <b>{element.displayName[key]}</b>
+                        </div>
+                    ) : null}
+                </div>
+            );
+        });
+    }
+
+    function generateMenuItems() {
+        return steps.map((element) => {
+            return (
+                <MenuItem onClick={(e) => handleCloseSelector(e, element.key)}>
+                    {/* {medStatus !== undefined
+                            ? statusIcons[medStatus]
+                       : null} */}
+                    <b className="selector-text">{element.displayName[key]}</b>
+                </MenuItem>
+            );
+        });
+    }
+
     return (
         <div className="toggle-buttons-wrapper">
             <ToggleButtonGroup
@@ -105,63 +136,7 @@ const ToggleButtons = ({
             </ToggleButtonGroup>
 
             <div className="toggle-button-selector">
-                {step === 'info' ? (
-                    <div className="current-step-label">
-                        {medStatus !== undefined
-                            ? statusIcons[medStatus]
-                            : null}
-                        <b>{lang.components.stepTabs.patientInfo}</b>
-                    </div>
-                ) : null}
-                {step === 'scan' ? (
-                    <div className="current-step-label">
-                        {earScanStatus !== undefined
-                            ? statusIcons[earScanStatus]
-                            : null}
-                        <b>{lang.components.stepTabs.earScan}</b>
-                    </div>
-                ) : null}
-                {step === 'cad' ? (
-                    <div className="current-step-label">
-                        {modelStatus !== undefined
-                            ? statusIcons[modelStatus]
-                            : null}
-                        <b>{lang.components.stepTabs.CADModeling}</b>
-                    </div>
-                ) : null}
-                {step === 'printing' ? (
-                    <div className="current-step-label">
-                        {printStatus !== undefined
-                            ? statusIcons[printStatus]
-                            : null}
-                        <b>{lang.components.stepTabs.print}</b>
-                    </div>
-                ) : null}
-                {step === 'processing' ? (
-                    <div className="current-step-label">
-                        {processingStatus !== undefined
-                            ? statusIcons[processingStatus]
-                            : null}
-                        <b>{lang.components.stepTabs.processing}</b>
-                    </div>
-                ) : null}
-                {step === 'delivery' ? (
-                    <div className="current-step-label">
-                        {deliveryStatus !== undefined
-                            ? statusIcons[deliveryStatus]
-                            : null}
-                        <b>{lang.components.stepTabs.delivery}</b>
-                    </div>
-                ) : null}
-                {step === 'feedback' ? (
-                    <div className="current-step-label">
-                        {feedbackStatus !== undefined
-                            ? statusIcons[feedbackStatus]
-                            : null}
-                        <b>{lang.components.stepTabs.feedback}</b>
-                    </div>
-                ) : null}
-
+                {generateLabels()}
                 <IconButton
                     aria-controls="customized-menu"
                     aria-haspopup="true"
@@ -176,70 +151,7 @@ const ToggleButtons = ({
                     open={Boolean(anchorEl)}
                     onClose={(e) => handleCloseSelector(e, 'close')}
                 >
-                    <MenuItem onClick={(e) => handleCloseSelector(e, 'info')}>
-                        {medStatus !== undefined
-                            ? statusIcons[medStatus]
-                            : null}
-                        <b className="selector-text">
-                            {lang.components.stepTabs.patientInfo}
-                        </b>
-                    </MenuItem>
-                    <MenuItem onClick={(e) => handleCloseSelector(e, 'scan')}>
-                        {earScanStatus !== undefined
-                            ? statusIcons[earScanStatus]
-                            : null}
-                        <b className="selector-text">
-                            {lang.components.stepTabs.earScan}
-                        </b>
-                    </MenuItem>
-                    <MenuItem onClick={(e) => handleCloseSelector(e, 'cad')}>
-                        {modelStatus !== undefined
-                            ? statusIcons[modelStatus]
-                            : null}
-                        <b className="selector-text">
-                            {lang.components.stepTabs.CADModeling}
-                        </b>
-                    </MenuItem>
-                    <MenuItem
-                        onClick={(e) => handleCloseSelector(e, 'printing')}
-                    >
-                        {printStatus !== undefined
-                            ? statusIcons[printStatus]
-                            : null}
-                        <b className="selector-text">
-                            {lang.components.stepTabs.print}
-                        </b>
-                    </MenuItem>
-                    <MenuItem
-                        onClick={(e) => handleCloseSelector(e, 'processing')}
-                    >
-                        {processingStatus !== undefined
-                            ? statusIcons[processingStatus]
-                            : null}
-                        <b className="selector-text">
-                            {lang.components.stepTabs.processing}
-                        </b>
-                    </MenuItem>
-                    <MenuItem
-                        onClick={(e) => handleCloseSelector(e, 'delivery')}
-                    >
-                        {deliveryStatus !== undefined
-                            ? statusIcons[deliveryStatus]
-                            : null}
-                        <b className="selector-text">
-                            {lang.components.stepTabs.delivery}
-                        </b>
-                    </MenuItem>
-                    <MenuItem
-                        onClick={(e) => handleCloseSelector(e, 'feedback')}
-                    >
-                        {feedbackStatus !== undefined
-                            ? statusIcons[feedbackStatus]
-                            : null}
-                        <b className="selector-text">
-                            {lang.components.stepTabs.feedback}
-                        </b>
-                    </MenuItem>
+                    {generateMenuItems()}
                 </Menu>
             </div>
         </div>
