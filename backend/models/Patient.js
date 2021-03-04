7efa65da7b255b0b32c0b151d8fe399db63b4832
Daniel <dm32@illinois.edu>
2021-03-04 10:47:56 -0600

    Add new patient schema

--- backend/models/Patient.js
+++ backend/models/Patient.js
@@ -21,7 +21,14 @@ const stageStatusEnum = {
 };
 
 // TODO: add / remove stage fields as needed
-const patientSchema = new mongoose.Schema({});
+const patientSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    dateCreated: { type: Date, required: true, default: new Date() },
+    orderId: { type: String, required: true },
+    lastEdited: { type: Date, required: true, default: new Date() },
+    lastEditedBy: { type: String, required: true },
+    status: { type: overallStatusEnum, required: true },
+});
 
 const Patient = mongoose.model('Patient', patientSchema);
 
