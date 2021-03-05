990e3266ff929d1f378771c52766de6feb7b8a53
Daniel <dm32@illinois.edu>
2021-03-04 21:49:06 -0600

    Remove file schema from patient

--- backend/models/Patient.js
+++ backend/models/Patient.js
@@ -1,11 +1,5 @@
 const mongoose = require('mongoose');
 
-const fileSchema = new mongoose.Schema({
-    filename: { type: String, required: true },
-    uploadedBy: { type: String, required: true },
-    uploadDate: { type: Date, required: true },
-});
-
 const overallStatusEnum = {
     ACTIVE: 'Active',
     ARCHIVED: 'Archived',
