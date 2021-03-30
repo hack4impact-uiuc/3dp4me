8e5c4cf06d13ea3930b6d0c992a328a20b6ec315
riyajain59 <riyaj3@illinois.edu>
2021-03-30 17:58:21 -0500

    Make slight change to display of fields

--- frontend/src/components/SectionTab/SectionTab.js
+++ frontend/src/components/SectionTab/SectionTab.js
@@ -2,14 +2,26 @@ import './SectionTab.css';
 import React, { useState, useEffect } from 'react';
 import { LanguageDataType } from '../../utils/custom-proptypes';
 import { getStageMetadata } from '../../utils/api';
-import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
 
 const SectionTab = ({ languageData }) => {
     const key = languageData.selectedLanguage;
     const [step, setStep] = useState('pateintInfo');
     const [stepMetadata, setStepMetadata] = useState([]);
+    //const [updatedData, setUpdatedData] = useState(_.cloneDeep(stepData));
+
+    const generateFields = () => {
+        if (stepMetadata == null || stepMetadata.fields == null) return null;
+        //if (updatedData == null) return null;
+        return stepMetadata.fields.map((field) => {
+            return (
+                <label>
+                    field.displayName;
+                    <input type="" name="" />
+                </label>
+            );
+        });
+    };
 
     useEffect(async () => {
         const stepMetadata = await getStageMetadata();
@@ -23,13 +35,14 @@ const SectionTab = ({ languageData }) => {
             <div className="sidebar">
                 <ListItem button> {element.displayName[key]} </ListItem>
                 <ListItem button></ListItem>
+                generateFields();
             </div>
         );
     });
+};
 
-    SectionTab.propTypes = {
-        languageData: LanguageDataType.isRequired,
-    };
+SectionTab.propTypes = {
+    languageData: LanguageDataType.isRequired,
 };
 
 export default SectionTab;
