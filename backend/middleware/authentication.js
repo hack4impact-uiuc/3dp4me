94d2ba69b44d2b993f5f191ce3139c3ccd899022
mlw6 <mattwalowski@gmail.com>
2021-04-01 20:10:00 -0500

    Parse user roles on auth

--- backend/middleware/authentication.js
+++ backend/middleware/authentication.js
@@ -1,30 +1,59 @@
 var AWS = require('aws-sdk');
-const { COGNITO_REGION } = require('../utils/aws/aws-exports');
+const {
+    COGNITO_REGION,
+    SECURITY_ROLE_ATTRIBUTE_NAME,
+} = require('../utils/aws/aws-exports');
 
 const requireAuthentication = async (req, res, next) => {
     try {
-        // TODO: Get more reliable way of accessing token
         const accessToken = req.headers.authorization.split(' ')[1];
 
         var params = {
             AccessToken: accessToken,
         };
 
-        // TODO: Should we create a new one each time, or should we hold a single global?
+        // Get the user's information
+        // TODO: Make this a global var?
         var cognitoidentityserviceprovider = new AWS.CognitoIdentityServiceProvider(
             { region: COGNITO_REGION },
         );
         const user = await cognitoidentityserviceprovider
             .getUser(params)
             .promise();
-        req.user = user;
 
+        // Get the security roles entry for the user.
+        const securityRolesString = user.UserAttributes.find(
+            (attribute) => attribute.Name === SECURITY_ROLE_ATTRIBUTE_NAME,
+        );
+
+        // If the user is missing the security roles entry, that means they have not been authorized
+        if (!securityRolesString)
+            return res.status(403).json({
+                success: false,
+                message:
+                    'You are not approved to access this site. Please contact an administrator.',
+            });
+
+        // Parse the roles into a JS array, and attach it to the request
+        const securityRoles = JSON.parse(securityRolesString.Value);
+        req.user = user;
+        req.user.roles = securityRoles;
         next();
     } catch (error) {
+        console.log(error);
         return res.status(401).json({
+            success: false,
             message: 'Authentication Failed',
         });
     }
+};
+
+const requireRole = async (req, res, next) => {
+    if (!req.user) await requireAuthentication();
+
+    // TODO: implement
+
+    next();
 };
 
 module.exports = requireAuthentication;
