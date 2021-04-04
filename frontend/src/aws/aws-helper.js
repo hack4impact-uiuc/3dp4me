34ee90b064f89445f85ee8d3db0519aa80a905df
mlw6 <mattwalowski@gmail.com>
2021-04-03 19:37:24 -0500

    Remove comment block

--- frontend/src/aws/aws-helper.js
+++ frontend/src/aws/aws-helper.js
@@ -61,16 +61,3 @@ export async function saveLanguagePreference(langKey) {
 export async function getCurrentSession() {
     return Auth.currentSession();
 }
-
-// TODO: Move this to the backend
-// export async function getAllUsers() {
-//     var params = {
-//         UserPoolId: awsconfig.Auth.userPoolId,
-//       };
-
-//       const cognitoidentityserviceprovider = new AWS.CognitoIdentityServiceProvider()
-//       cognitoidentityserviceprovider.listUsers(params, function(err, data) {
-//         if (err) console.log(err, err.stack); // an error occurred
-//         else     console.log(data);           // successful response
-//       });
-// }
