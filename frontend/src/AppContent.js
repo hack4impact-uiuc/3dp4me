5f249233fc30ab53e827cac98aa327d2ef30e687
mlw6 <mattwalowski@gmail.com>
2021-04-10 13:45:02 -0500

    Add modal root ref

---
+++ frontend/src/AppContent.js
@@ -0,0 +1,93 @@
+import React, { useContext } from 'react';
+import { Modal, RootRef } from '@material-ui/core';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Dashboard from './pages/Dashboard/Dashboard';
+import AccountManagement from './pages/AccountManagement/AccountManagment';
+import Metrics from './pages/Metrics/Metrics';
+import Patients from './pages/Patients/Patients';
+import Navbar from './components/Navbar/Navbar';
+import SectionTab from './components/SectionTab/SectionTab';
+import Controller from './steps/Controller/Controller';
+import { REDUCER_ACTIONS } from './utils/constants';
+import { Context } from './store/Store';
+import { useErrorWrap } from './hooks/useErrorWrap';
+
+function AppContent({ languageData, onLanguageChange, username, userEmail }) {
+    const [state, dispatch] = useContext(Context);
+    useErrorWrap(() => {
+        throw 'Error occurred';
+    });
+
+    const key = languageData.selectedLanguage;
+    const lang = languageData.translations[key];
+
+    const ErrorModal = ({ message, isOpen, onClose }) => {
+        return (
+            <Modal
+                open={isOpen}
+                onClose={onClose}
+                container={() => RootRef.current}
+            >
+                <div>
+                    <h1>TESTESTS</h1>
+                    <h1>{message}</h1>
+                </div>
+            </Modal>
+        );
+    };
+
+    const handleErrorModalClose = () => {
+        dispatch({ action: REDUCER_ACTIONS.CLEAR_ERROR });
+    };
+
+    console.log(state.isErrorVisible);
+
+    const renderAppContent = () => {
+        // TODO: Add some state for if the user can't authenticate
+        return (
+            <div dir={key === 'AR' ? 'rtl' : 'ltr'}>
+                <Router>
+                    <Navbar
+                        languageData={languageData}
+                        setSelectedLang={onLanguageChange}
+                        username={username}
+                        userEmail={userEmail}
+                    />
+                    <div className={`${key === 'AR' ? 'flip' : ''} content`}>
+                        <Switch>
+                            <Route exact path="/">
+                                <ErrorModal
+                                    message={state.errorMessage}
+                                    isOpen={state.isErrorVisible}
+                                    onClose={handleErrorModalClose}
+                                />
+                                <Dashboard languageData={languageData} />
+                            </Route>
+                            <Route exact path="/account">
+                                <AccountManagement
+                                    languageData={languageData}
+                                />
+                            </Route>
+                            <Route exact path="/metrics">
+                                <Metrics languageData={languageData} />
+                            </Route>
+                            <Route exact path="/patients">
+                                <Patients languageData={languageData} />
+                            </Route>
+                            <Route exact path="/patient-info/:patientId">
+                                <Controller languageData={languageData} />
+                            </Route>
+                            <Route exact path="/section-tab">
+                                <SectionTab languageData={languageData} />
+                            </Route>
+                        </Switch>
+                    </div>
+                </Router>
+            </div>
+        );
+    };
+
+    return renderAppContent();
+}
+
+export default AppContent;
