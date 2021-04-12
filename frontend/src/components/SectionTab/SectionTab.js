6fae24ae4e87918c0057e8b69bbd813b22dbbfcd
riyajain59 <riyaj3@illinois.edu>
2021-04-12 15:07:05 -0500

    Create cards for section tabs

--- frontend/src/components/SectionTab/SectionTab.js
+++ frontend/src/components/SectionTab/SectionTab.js
@@ -1,15 +1,33 @@
 import './SectionTab.css';
 import React, { useState, useEffect } from 'react';
-import ListItem from '@material-ui/core/ListItem';
 
 import { LanguageDataType } from '../../utils/custom-proptypes';
 import { getAllStepsMetadata } from '../../utils/api';
-import CreateFieldModal from '../CreateFieldModal/CreateFieldModal';
+import Sidebar from '../Sidebar/Sidebar';
+import StepManagementContent from '../StepManagementContent/StepManagementContent';
 
 const SectionTab = ({ languageData }) => {
     const key = languageData.selectedLanguage;
     const [stepMetadata, setStepMetadata] = useState([]);
-    const [modalOpen, setModalOpen] = useState(false);
+    const [selectedStep, setSelectedStep] = useState('');
+
+    function GenerateStepManagementContent() {
+        return stepMetadata.map((element) => {
+            if (selectedStep != element.key) {
+                return null;
+            }
+            return (
+                <StepManagementContent
+                    languageData={languageData}
+                    fields={element.fields}
+                />
+            );
+        });
+    }
+
+    function UpdateSelectedStep(stepKey) {
+        setSelectedStep(stepKey);
+    }
 
     useEffect(() => {
         const fetchData = async () => {
@@ -17,49 +35,23 @@ const SectionTab = ({ languageData }) => {
             if (metadata != null) {
                 setStepMetadata(metadata);
             }
+            if (metadata.length > 1) {
+                setSelectedStep(metadata[0].key);
+            }
         };
         fetchData();
     }, [setStepMetadata]);
 
-    const generateSteps = () => {
-        return stepMetadata.map((element) => {
-            return (
-                <div className="sidebar">
-                    <ListItem button> {element.displayName[key]} </ListItem>
-                </div>
-            );
-        });
-    };
-
-    const onModalClose = () => {
-        setModalOpen(false);
-    };
-
-    const generateNewFieldPopup = () => {
-        return (
-            <CreateFieldModal
-                isOpen={modalOpen}
-                onModalClose={onModalClose}
-                languageData={languageData}
-            />
-        );
-    };
-
     return (
-        <div>
-            <span> {generateSteps()}</span>
+        <div className="dashboard section-management-container">
             <div>
-                <ListItem
-                    className="sidebar"
-                    button
-                    onClick={() => {
-                        setModalOpen(true);
-                    }}
-                >
-                    Add New Field
-                </ListItem>
+                <Sidebar
+                    languageData={languageData}
+                    onClick={UpdateSelectedStep}
+                    stepMetadata={stepMetadata}
+                />
             </div>
-            {generateNewFieldPopup()}
+            {GenerateStepManagementContent()}
         </div>
     );
 };
