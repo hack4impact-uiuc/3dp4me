0eb95fe11c55c8e3ef1cf2ea0672068ab9918f15
mlw6 <mattwalowski@gmail.com>
2021-04-30 10:24:54 -0500

    Fix bug

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -182,7 +182,6 @@ router.put(
 // Deletes user role
 router.delete(
     '/:username/roles/:roleId',
-    RequireAdmin,
     errorWrap(async (req, res) => {
         const { username, roleId } = req.params;
         const userRoles = await getUserRoles(username);
