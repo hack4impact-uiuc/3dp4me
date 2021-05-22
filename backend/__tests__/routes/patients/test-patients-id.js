ae968af3a259d8604bda522d63a3bbcd33a3015b
Daniel Moon <dm32@illinois.edu>
2021-05-22 07:13:06 -0400

    Add tests for endpoint after changing metadata

--- backend/__tests__/routes/patients/test-patients-id.js
+++ backend/__tests__/routes/patients/test-patients-id.js
@@ -95,4 +95,65 @@ describe('GET /patient/:id', () => {
             resContent.result,
         );
     });
+    
+    it('get patient with no step data and no steps in collection', async () => {
+        // Delete all steps
+        let updatedData = await mongoose.connection.collection('steps').deleteMany({});
+        expect(updatedData.deletedCount).toBe(3);
+
+        // Delete model
+        delete mongoose.connection.models['survey'];
+        delete mongoose.connection.models['example'];
+        delete mongoose.connection.models['medicalInfo'];
+
+        const patientID = '60944e084f4c0d4330cc25ec';
+        const res = await withAuthentication(
+            request(server).get(`/api/patients/${patientID}`),
+        );
+
+        // Check statuses are correct
+        const resContent = JSON.parse(res.text);
+        expect(res.status).toBe(200);
+        expect(resContent.success).toBe(true);
+        
+        // Delete removed steps from expected
+        delete GET_PATIENT_WITHOUT_STEP_DATA.survey
+        delete GET_PATIENT_WITHOUT_STEP_DATA.example
+        delete GET_PATIENT_WITHOUT_STEP_DATA.medicalInfo
+
+        // Check responses
+        expectStrictEqualWithTimestampOrdering(
+            GET_PATIENT_WITHOUT_STEP_DATA,
+            resContent.result,
+        );
+    });
+
+    it('get patient with some step data and some steps in collection', async () => {
+        // Delete one step
+        let updatedData = await mongoose.connection.collection('steps').deleteOne({key: "example"})
+        expect(updatedData.deletedCount).toBe(1);
+
+        // Delete model
+        delete mongoose.connection.models['example'];
+
+        const patientID = '60944e084f4c0d4330cc25ee';
+        const res = await withAuthentication(
+            request(server).get(`/api/patients/${patientID}`),
+        );
+
+        // Check statuses are correct
+        const resContent = JSON.parse(res.text);
+        expect(res.status).toBe(200);
+        expect(resContent.success).toBe(true);
+        
+        // Delete removed steps from expected
+        delete GET_PATIENT_WITH_SOME_STEP_DATA.example
+
+        console.log(GET_PATIENT_WITH_SOME_STEP_DATA);
+        // Check responses
+        expectStrictEqualWithTimestampOrdering(
+            GET_PATIENT_WITH_SOME_STEP_DATA,
+            resContent.result,
+        );
+    });
 });
