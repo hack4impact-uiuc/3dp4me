1955a120690aaeb2aa06592c6e1d365e3d9e17ef
Daniel Moon <dm32@illinois.edu>
2021-06-03 12:11:09 -0400

    add non-passing tests

--- backend/__tests__/routes/steps/test-steps-stepkey.js
+++ backend/__tests__/routes/steps/test-steps-stepkey.js
@@ -2,6 +2,7 @@ const db = require('../../utils/db');
 const _ = require('lodash');
 const request = require('supertest');
 const AWS = require('aws-sdk-mock');
+const mongoose = require('mongoose');
 var server = require('../../../app');
 const {
     initAuthMocker,
@@ -14,14 +15,14 @@ const {
     areObjectsDisjoint,
 } = require('../../utils/utils');
 const {
-    POST_PATIENT,
-    POST_PATIENT_MINIMAL_REQUEST,
-    DEFAULT_PATIENT_DATA,
-    POST_IMMUTABLE_PATIENT_DATA,
-} = require('../../mock-data/patients-mock-data');
+    PUT_STEP_REORDERED_FIELDS,
+    PUT_STEP_REORDERED_FIELDS_EXPECTED
+} = require('../../mock-data/steps-mock-data');
 const { models } = require('../../../models');
+const { Mongoose } = require('mongoose');
 
 describe('PUT /steps/stepkey', () => {
+    const STEP_KEY = 'survey';
     afterAll(async () => await db.closeDatabase());
     afterEach(async () => await db.resetDatabase());
     beforeAll(async () => {
@@ -34,5 +35,41 @@ describe('PUT /steps/stepkey', () => {
         server = require('../../../app');
     });
 
-    it('')
+    it('given bad stepkey, return 404', (done) => {
+        const invalidStepKey = "DoesNotExist";
+        withAuthentication(
+            request(server)
+            .put(`/api/metadata/steps/${invalidStepKey}`)
+        ).expect(404, done);
+    });
+
+
+    it ('reorder fields in step', async () => {
+        const stepID = '6092c26fe0912601bbc5d85d';
+
+        const res = await withAuthentication(
+            request(server)
+                .put(`/api/metadata/steps/${STEP_KEY}`)
+                .send(PUT_STEP_REORDERED_FIELDS)
+        );
+
+        console.log(res.text);
+
+        // Check response
+        const resContent = JSON.parse(res.text);
+        expect(resContent.status).toBe(200);
+        expect(resContent.success).toBe(true);
+        expectStrictEqualWithTimestampOrdering(
+            PUT_STEP_REORDERED_FIELDS_EXPECTED, 
+            resContent.result,
+        );
+
+        // Check database
+        let updatedData = await mongoose.connection
+            .collection('steps')
+            .findOne({_id: mongoose.Types.ObjectId(stepID)});
+        
+        expectStrictEqualWithTimestampOrdering(PUT_STEP_REORDERED_FIELDS_EXPECTED, updatedData);
+
+    });
 });
