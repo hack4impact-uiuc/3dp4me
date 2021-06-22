1094f2ef43277cc83211ce6ba2d8259dfc588fc2
mlw6 <mattwalowski@gmail.com>
2021-06-21 19:26:06 -0500

    Update shell command

--- check-sensitive-files.sh
+++ check-sensitive-files.sh
@@ -1,10 +1,14 @@
+#!/bin/sh
+
 ROOT_DIR="$(pwd)/"
 LIST=$(git diff --cached --name-only --diff-filter=ACRM)
 
 for file in $LIST
 do
-    if [[ $file == *.env ]]; then
+    if [[ "${file: -4}" == *.env ]]; then
+        echo
         echo You cannot commit file $file, you might publish our AWS secrets! To ignore all future changes to $file, run the following command:
+        echo
         echo git update-index --assume-unchanged $file
         exit 1
     fi
