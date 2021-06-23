1e9988d5a0e3765d696e11dc336b8c46b3ad3fd0
Matthew Walowski <mattwalowski@gmail.com>
2021-06-23 07:17:10 -0500

    Seperate environments (#244)
    * Modify env files
    
    * Add seperate S3
    
    * Add template .env files
    
    * Update .gitignore
    
    * Rename template .env files so that build will take them
    
    * Add template .env for frontend
    
    * Only run frontend/backend suite if files changed
    
    * Run job on push/pr to master
    
    * Save empty env files
    
    * Add script to check sensitive files
    
    * Match all *.env files in script
    
    * Update shell command
    
    * Add empty env files for frontend
    
    * Update suggested command
    
    * Remove diff checking for actions because they will not run if last failed
    
    * Add deleted jobs tag
    
    * Ahhhhh I keep breaking things
    
    * add test env file
    
    * Force exit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import { Amplify, Auth } from 'aws-amplify';
 import { registerLocale } from 'react-datepicker';
 import { enUS, arSA } from 'date-fns/locale';
-
 import Store from './store/Store';
 import AppContent from './AppContent';
 import { awsconfig } from './aws/aws-exports';
