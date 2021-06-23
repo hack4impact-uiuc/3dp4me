6209e894ab5e7fe5d3db55ece8850593023224ab
mlw6 <mattwalowski@gmail.com>
2021-06-23 08:02:12 -0500

    Change update instructions

--- scripts/check-sensitive-files.sh
+++ scripts/check-sensitive-files.sh
@@ -7,9 +7,8 @@ for file in $LIST
 do
     if [[ "${file: -4}" == *.env ]]; then
         echo
-        echo You cannot commit file $file, you might publish our AWS secrets! To ignore all future changes to $file, run the following command:
-        echo
-        echo git reset && git update-index --assume-unchanged $file
+        echo You cannot commit file $file! Did you run the init script?
+        echo From the root of the repo, run yarn init-repo
         exit 1
     fi
 done
