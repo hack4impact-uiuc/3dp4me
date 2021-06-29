4c7f994211061e42d514858816ce82e596580a2e
Daniel Moon <dm32@illinois.edu>
2021-06-29 17:48:46 -0400

    clean up code

--- backend/middleware/authentication.js
+++ backend/middleware/authentication.js
@@ -49,10 +49,8 @@ const parseUserAccess = (user) => {
 
 const requireAuthentication = async (req, res, next) => {
     try {
-        console.log(req.headers.authorization);
         const accessToken = req.headers.authorization.split(' ')[1];
         const user = await getUser(accessToken);
-        console.log(user);
         user.roles = parseUserSecurityRoles(user);
         user.accessLevel = parseUserAccess(user);
 
