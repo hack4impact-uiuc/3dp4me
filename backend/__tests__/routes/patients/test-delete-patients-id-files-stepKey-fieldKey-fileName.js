ed21195c9db8b05b504f6c8be21ac6986e4969ad
Vasu Chalasani <vasudhachalasani@gmail.com>
2021-07-08 20:09:50 -0500

    Finish tests

--- backend/__tests__/routes/patients/test-delete-patients-id-files-stepKey-fieldKey-fileName.js
+++ backend/__tests__/routes/patients/test-delete-patients-id-files-stepKey-fieldKey-fileName.js
@@ -2,6 +2,8 @@ const db = require('../../utils/db');
 const _ = require('lodash');
 const request = require('supertest');
 const AWS = require('aws-sdk-mock');
+const mongoose = require('mongoose');
+
 var server = require('../../../app');
 const {
     initAuthMocker,
@@ -27,45 +29,49 @@ describe('DELETE /patients/:id/files/:stepKey/:fieldKey/:fileName #214', () => {
         server = require('../../../app');
     });
 
-    // it('returns 404 when given bad ID format', (done) => {
-    //     withAuthentication(
-    //         request(server).delete(
-    //             `/api/patients/badid/${STEP_KEY}/file/filename`,
-    //         ),
-    //     ).expect(404, done);
-    // });
+    it('returns 404 when given bad ID format', (done) => {
+        withAuthentication(
+            request(server).delete(
+                `/api/patients/badid/${STEP_KEY}/file/filename`,
+            ),
+        ).expect(404, done);
+    });
 
-    // it('returns 404 when given nonexistent ID', (done) => {
-    //     const randID = '6092a9ae9e3769ae75abe0a5';
-    //     withAuthentication(
-    //         request(server).delete(
-    //             `/api/patients/${randID}/${STEP_KEY}/file/filename`,
-    //         ),
-    //     ).expect(404, done);
-    // });
+    it('returns 404 when given nonexistent ID', (done) => {
+        const randID = '6092a9ae9e3769ae75abe0a5';
+        withAuthentication(
+            request(server).delete(
+                `/api/patients/${randID}/${STEP_KEY}/file/filename`,
+            ),
+        ).expect(404, done);
+    });
 
-    // it('returns 404 when given bad stepKey', (done) => {
-    //     withAuthentication(
-    //         request(server).delete(
-    //             `/api/patients/${PATIENT_ID_MISSING_DATA}/badstep/file/filename`,
-    //         ),
-    //     ).expect(404, done);
-    // });
+    it('returns 404 when given bad stepKey', (done) => {
+        withAuthentication(
+            request(server).delete(
+                `/api/patients/${PATIENT_ID_MISSING_DATA}/badstep/file/filename`,
+            ),
+        ).expect(404, done);
+    });
 
     it('delete one file from step 2', async () => {
         const STEP_KEY = 'example';
         const FIELD_KEY = 'file';
         const PATIENT_ID = '60944e084f4c0d4330cc258b';
         const FILE_NAME = 'utilisation_modular.ssf';
+        const url = `api/patients/${PATIENT_ID}/files/${STEP_KEY}/${FIELD_KEY}/${FILE_NAME}`;
+        console.log(url);
 
         const res = await withAuthentication(
             request(server).delete(
-                `api/patient/${PATIENT_ID}/files/${STEP_KEY}/${FIELD_KEY}/${FILE_NAME}`,
+                `/api/patients/${PATIENT_ID}/files/${STEP_KEY}/${FIELD_KEY}/${FILE_NAME}`,
             ),
         );
-        // expect(res.status).toBe(200);
+        expect(res.status).toBe(200);
 
-        // const step = await db.collection('example').findOne({ "patientId": PATIENT_ID});
-        // expect(step.file).toBe([]);
+        const step = await mongoose.connection.db
+            .collection('example')
+            .findOne({ patientId: PATIENT_ID });
+        expect(step.file).toStrictEqual([]);
     });
 });
