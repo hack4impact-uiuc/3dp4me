4314223fb4b2caef798458652a73805cd3ce840b
mlw6 <mattwalowski@gmail.com>
2021-07-13 17:52:57 -0500

    Add rate limiter

--- backend/app.js
+++ backend/app.js
@@ -10,9 +10,11 @@ const bodyParser = require('body-parser');
 const { errorHandler } = require('./utils');
 const { requireAuthentication } = require('./middleware/authentication');
 const { initDB } = require('./utils/init-db');
+const rateLimiter = require('./middleware/rateLimiter');
 const app = express();
 
 app.use(helmet());
+app.use(rateLimiter);
 app.use(express.static(path.join(__dirname, '../frontend/build')));
 app.use(cors());
 
