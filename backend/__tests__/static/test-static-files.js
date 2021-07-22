30d09764d8cd2880ad4c1a658ebda6c897e479ae
Matthew Walowski <mattwalowski@gmail.com>
2021-07-22 09:39:52 -0500

    Test static headers

---
+++ backend/__tests__/static/test-static-files.js
@@ -0,0 +1,31 @@
+let server = require('../../app');
+const db = require('../utils/db');
+const _ = require('lodash');
+const AWS = require('aws-sdk-mock');
+const request = require('supertest');
+const omitDeep = require('omit-deep-lodash');
+const { initAuthMocker, setCurrentUser } = require('../utils/auth');
+const { getFieldByKey } = require('../../utils/step-utils');
+const { models } = require('../../models');
+
+describe('Get statically served files', () => {
+    afterAll(async () => await db.closeDatabase());
+    afterEach(async () => await db.resetDatabase());
+    beforeAll(async () => {
+        await db.connect();
+        initAuthMocker(AWS);
+        setCurrentUser(AWS);
+    });
+
+    beforeEach(() => {
+        server = require('../../app');
+    });
+
+    it('uses cross origin isolation', async () => {
+        const res = await request(server).get(`/`);
+
+        // Need these headers for SharedArrayBuffer to work in newer browsers
+        expect(res.header['cross-origin-embedder-policy']).toBe('require-corp');
+        expect(res.header['cross-origin-opener-policy']).toBe('same-origin');
+    });
+});
