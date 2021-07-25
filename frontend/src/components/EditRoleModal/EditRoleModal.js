679e54bbe8b4077c9b551e42468ed2fba81bc8cb
Matthew Walowski <mattwalowski@gmail.com>
2021-07-24 21:48:44 -0500

    User dashboard (#252)
    * Copied over Kelly's changes
    
    * Open modal on user click
    
    * Update role schema
    
    * Get roles to display names instead of IDs
    
    * Style modal
    
    * Style text fields
    
    * Update user on save
    
    * Get users to be updated immediately
    
    * Fix role updating
    
    * Fix lint errors

--- frontend/src/components/EditRoleModal/EditRoleModal.js
+++ frontend/src/components/EditRoleModal/EditRoleModal.js
@@ -1,4 +1,9 @@
-import React, { useState } from 'react';
+// Disabled because we need the role updates to happen sequentially
+/* eslint-disable no-await-in-loop */
+// More readabe without this
+/* eslint-disable no-lonely-if */
+
+import React, { useEffect, useState } from 'react';
 import _ from 'lodash';
 import {
     Modal,
@@ -14,18 +19,25 @@ import TextField from '../Fields/TextField';
 import MultiSelectField from '../Fields/MultiSelectField';
 import { ACCESS_LEVELS } from '../../utils/constants';
 import './EditRoleModal.scss';
+import { addUserRole, removeUserRole, setUserAccess } from '../../utils/api';
+import { useErrorWrap } from '../../hooks/useErrorWrap';
 
 const EditRoleModal = ({
     languageData,
     isOpen,
     onClose,
+    onUserEdited,
     userInfo,
     allRoles,
 }) => {
     const [userData, setUserData] = useState(_.cloneDeep(userInfo));
-
+    const errorWrap = useErrorWrap();
     const key = languageData.selectedLanguage;
     const lang = languageData.translations[key];
+
+    useEffect(() => {
+        setUserData(_.cloneDeep(userInfo));
+    }, [userInfo]);
 
     const onRolesChange = (id, roles) => {
         setUserData({ ...userData, roles });
@@ -35,22 +47,52 @@ const EditRoleModal = ({
         setUserData({ ...userData, accessLevel: event.target.value });
     };
 
-    const onSave = (event) => {
-        // TODO: Make post requests and callback to parent
-        console.log(event);
+    const onSave = async () => {
+        // Update users roles
+        for (let i = 0; i < allRoles.length; i += 1) {
+            const role = allRoles[i];
+
+            // If user has role
+            if (userData.roles.find((r) => r === role._id)) {
+                // If user didn't have role before, make request to backend
+                if (!userInfo.roles.find((r) => r === role._id)) {
+                    await errorWrap(async () =>
+                        addUserRole(userData.userName, role._id),
+                    );
+                }
+            } else {
+                // If user did have role before, make request to backend
+                if (userInfo.roles.find((r) => r === role._id)) {
+                    await errorWrap(async () =>
+                        removeUserRole(userData.userName, role._id),
+                    );
+                }
+            }
+        }
+
+        // Update user access level
+        await errorWrap(async () =>
+            setUserAccess(userData.userName, userData.accessLevel),
+        );
+
+        // Close modal and update local data
+        onClose();
+        onUserEdited(userData.userName, userData.accessLevel, userData.roles);
     };
 
     return (
-        <Modal open={isOpen} onClose={onClose}>
+        <Modal open={isOpen} onClose={onClose} className="edit-role-modal">
             <div className="edit-role-modal-wrapper">
                 <h2>{lang.accountManagement.editAccount}</h2>
                 <TextField
+                    className="text-field"
                     displayName={lang.accountManagement.username}
                     type="text"
                     isDisabled
                     value={userData?.userName}
                 />
                 <TextField
+                    className="text-field"
                     displayName={lang.accountManagement.email}
                     type="text"
                     isDisabled
@@ -83,10 +125,10 @@ const EditRoleModal = ({
                     </Select>
                 </FormControl>
                 <div>
-                    <Button onClick={onSave}>
+                    <Button className="save-user-button" onClick={onSave}>
                         {lang.accountManagement.Save}
                     </Button>
-                    <Button onClick={onClose}>
+                    <Button className="discard-user-button" onClick={onClose}>
                         {lang.accountManagement.Discard}
                     </Button>
                 </div>
@@ -98,6 +140,7 @@ const EditRoleModal = ({
 EditRoleModal.propTypes = {
     isOpen: PropTypes.bool.isRequired,
     onClose: PropTypes.func.isRequired,
+    onUserEdited: PropTypes.func.isRequired,
     allRoles: PropTypes.arrayOf(PropTypes.string),
     languageData: LanguageDataType.isRequired,
     userInfo: PropTypes.shape({
---
+++ frontend/src/components/Table/MainUserTable.js
@@ -0,0 +1,221 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import PropTypes from 'prop-types';
+import { IconButton } from '@material-ui/core';
+import './MainTable.scss';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import ArrowDropUpIcon from '@material-ui/icons/ArrowDropUp';
+
+import useSortableData from '../../hooks/useSortableData';
+import Eyecon from '../../assets/view.svg';
+import {
+    LanguageDataType,
+    TableHeaderType,
+} from '../../utils/custom-proptypes';
+import { ACCESS_LEVELS } from '../../utils/constants';
+
+const StyledTableCell = withStyles((theme) => ({
+    head: {
+        backgroundColor: theme.palette.common.white,
+        color: theme.palette.common.black,
+    },
+}))(TableCell);
+
+const StyledTableRow = withStyles(() => ({
+    root: {
+        '&:hover': {
+            backgroundColor: '#f0f0f0',
+        },
+    },
+}))(TableRow);
+
+const MainUserTable = ({
+    languageData,
+    users,
+    roleData,
+    headers,
+    onUserSelected,
+}) => {
+    const key = languageData.selectedLanguage;
+    const lang = languageData.translations[key];
+    const UNSORTED_DATA = users;
+    const { items, requestSort, sortConfig } = useSortableData(
+        users,
+        UNSORTED_DATA,
+    );
+
+    const statusStyle = {
+        [ACCESS_LEVELS.GRANTED]: (
+            <div style={{ color: '#65d991' }}>
+                <b>{lang.accountManagement.Approved}</b>
+            </div>
+        ),
+        [ACCESS_LEVELS.PENDING]: (
+            <div style={{ color: 'black' }}>
+                <b>{lang.accountManagement.Pending}</b>
+            </div>
+        ),
+        [ACCESS_LEVELS.REVOKED]: (
+            <div style={{ color: 'red' }}>
+                <b>{lang.accountManagement.Revoked}</b>
+            </div>
+        ),
+    };
+
+    const getInfo = (user, atr) => {
+        return user?.Attributes?.find((attribute) => attribute.Name === atr)
+            ?.Value;
+    };
+
+    const getAccessLevelValue = (user) => {
+        return getInfo(user, 'custom:access') || ACCESS_LEVELS.PENDING;
+    };
+
+    const getAccessLevel = (user) => {
+        const access = getAccessLevelValue(user);
+        return statusStyle[access];
+    };
+
+    const getName = (user) => {
+        return getInfo(user, 'name') || user.Username;
+    };
+
+    const getRolesValue = (user) => {
+        const info = getInfo(user, 'custom:security_roles');
+        return info ? JSON.parse(info) : [];
+    };
+
+    const getRoles = (user) => {
+        let roles = getRolesValue(user);
+        if (roles.length === 0) return 'Not Assigned';
+
+        roles = roles.map((r) => {
+            for (let i = 0; i < roleData.length; i += 1) {
+                if (r === roleData[i]._id) return roleData[i]?.Question[key];
+            }
+
+            return 'Unrecognized role';
+        });
+
+        return roles.join(', ');
+    };
+
+    const onSelected = (item) => {
+        onUserSelected({
+            userName: getName(item),
+            userEmail: getInfo(item, 'email'),
+            roles: getRolesValue(item),
+            accessLevel: getAccessLevelValue(item),
+        });
+    };
+
+    return (
+        <div className="table-container">
+            <TableContainer className="table-container" component={Paper}>
+                <Table stickyHeader className="table">
+                    <TableHead>
+                        <TableRow>
+                            {headers.map((header) => (
+                                <StyledTableCell
+                                    onClick={() => requestSort(header.sortKey)}
+                                    className="header"
+                                    key={header}
+                                    align={key === 'AR' ? 'right' : 'left'}
+                                >
+                                    <div
+                                        className={
+                                            key === 'AR'
+                                                ? 'cell-align-rtl'
+                                                : 'cell-align'
+                                        }
+                                    >
+                                        {header}
+                                        {sortConfig !== null &&
+                                        sortConfig.key === header.sortKey &&
+                                        sortConfig.direction === 'ascending' ? (
+                                            <ArrowDropUpIcon className="dropdown-arrow" />
+                                        ) : (
+                                            <></>
+                                        )}
+                                        {sortConfig !== null &&
+                                        sortConfig.key === header.sortKey &&
+                                        sortConfig.direction ===
+                                            'descending' ? (
+                                            <ArrowDropDownIcon className="dropdown-arrow" />
+                                        ) : (
+                                            <></>
+                                        )}
+                                    </div>
+                                </StyledTableCell>
+                            ))}
+                            <StyledTableCell
+                                className="header"
+                                align="center"
+                            />
+                        </TableRow>
+                    </TableHead>
+                    <TableBody className="table-body">
+                        {items.map((user) => (
+                            <StyledTableRow key={user._id}>
+                                <StyledTableCell>
+                                    {getName(user)}
+                                </StyledTableCell>
+                                <StyledTableCell>
+                                    {getInfo(user, 'email')}
+                                </StyledTableCell>
+                                <StyledTableCell>
+                                    {getRoles(user)}
+                                </StyledTableCell>
+                                <StyledTableCell>
+                                    {getAccessLevel(user)}
+                                </StyledTableCell>
+                                <StyledTableCell
+                                    className="cell"
+                                    align="center"
+                                >
+                                    <IconButton
+                                        onClick={() => onSelected(user)}
+                                    >
+                                        <img
+                                            alt="status icon"
+                                            width="18px"
+                                            src={Eyecon}
+                                        />
+                                    </IconButton>{' '}
+                                    {lang.components.table.edit}
+                                </StyledTableCell>
+                            </StyledTableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </div>
+    );
+};
+
+MainUserTable.propTypes = {
+    languageData: LanguageDataType.isRequired,
+    headers: PropTypes.arrayOf(TableHeaderType).isRequired,
+    onUserSelected: PropTypes.func.isRequired,
+    users: PropTypes.arrayOf(PropTypes.object),
+
+    // roleData is in this format so that it is consumable by multiselect
+    roleData: PropTypes.arrayOf(
+        PropTypes.shape({
+            _id: PropTypes.string,
+            Question: PropTypes.shape({
+                EN: PropTypes.string,
+                AR: PropTypes.AR,
+            }),
+        }),
+    ),
+};
+
+export default MainUserTable;
