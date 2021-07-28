8a139a377360c5058355b1f734d28585bddefeb3
Matthew Walowski <mattwalowski@gmail.com>
2021-07-28 17:49:26 -0500

    Add comments to hooks

--- frontend/src/hooks/useErrorWrap.js
+++ frontend/src/hooks/useErrorWrap.js
@@ -3,6 +3,11 @@ import { useContext, useCallback } from 'react';
 import { Context } from '../store/Store';
 import { REDUCER_ACTIONS } from '../utils/constants';
 
+/**
+ * Custom hook that creates an error wrapper. If an error is thrown in the
+ * error wrapper, then the global error is set and the error modal automatically
+ * appears over the screen
+ */
 export const useErrorWrap = () => {
     const dispatch = useContext(Context)[1];
     const errorWrapper = useCallback(
