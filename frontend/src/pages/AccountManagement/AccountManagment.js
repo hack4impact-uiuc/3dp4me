0be45f6e10e72e171754ac73e12a2dbda00986ed
Matthew Walowski <mattwalowski@gmail.com>
2021-07-28 17:10:32 -0500

    Cleaned up account management page

--- frontend/src/pages/AccountManagement/AccountManagment.js
+++ frontend/src/pages/AccountManagement/AccountManagment.js
@@ -25,6 +25,20 @@ import {
     getUsername,
 } from '../../aws/aws-users';
 
+const USER_TABLE_HEADERS = [
+    { title: 'Name', sortKey: 'Name' },
+    { title: 'Email', sortKey: 'Email' },
+    { title: 'Roles', sortKey: 'Roles' },
+    { title: 'Access', sortKey: 'Access' },
+];
+
+const USER_TABLE_ROW_DATA = [
+    { id: USER_TABLE_HEADERS[0].sortKey, dataType: FIELD_TYPES.STRING },
+    { id: USER_TABLE_HEADERS[1].sortKey, dataType: FIELD_TYPES.STRING },
+    { id: USER_TABLE_HEADERS[2].sortKey, dataType: FIELD_TYPES.STRING },
+    { id: USER_TABLE_HEADERS[3].sortKey, dataType: FIELD_TYPES.ACCESS },
+];
+
 const AccountManagement = () => {
     const selectedLang = useTranslations()[1];
     const [userMetaData, setUserMetaData] = useState([]);
@@ -38,47 +52,83 @@ const AccountManagement = () => {
             setUserMetaData(userRes.result.Users);
 
             const rolesRes = await getAllRoles();
-            const roles = rolesRes.result.map((r) => ({
-                _id: r?._id,
-                IsHidden: r?.isHidden,
-                Question: r?.roleName,
-            }));
+            const roles = rolesToMultiSelectFormat(rolesRes.result);
 
             setRolesData(roles);
         };
         errorWrap(fetchData);
     }, [setUserMetaData, errorWrap]);
 
-    const onUserSelected = (user) => {
-        const fullUserData = userMetaData.find(
-            (u) => u.Username === user.Username,
-        );
-
-        setSelectedUser({
-            accessLevel: getAccessLevel(fullUserData),
-            userId: getUsername(fullUserData),
-            userName: getName(fullUserData),
-            userEmail: getEmail(fullUserData),
-            roles: getRolesValue(fullUserData),
-        });
+    /**
+     * Converts the roles response to a format useable by the MultiSelect field
+     */
+    const rolesToMultiSelectFormat = (roles) => {
+        return roles.map((r) => ({
+            _id: r?._id,
+            IsHidden: r?.isHidden,
+            Question: r?.roleName,
+        }));
     };
 
+    /**
+     * Formats a user to a format useable by the EditRoleModal
+     */
+    const userToRoleModalFormat = (user) => {
+        return {
+            accessLevel: getAccessLevel(user),
+            userId: getUsername(user),
+            userName: getName(user),
+            userEmail: getEmail(user),
+            roles: getRolesValue(user),
+        };
+    };
+
+    /**
+     * Formats the users response to be useable by the table
+     */
+    const usersToTableFormat = (users) => {
+        return users.map((user) => ({
+            Username: getUsername(user),
+            Name: getName(user),
+            Email: getEmail(user),
+            Roles: getRoles(user, rolesData, selectedLang),
+            Access: getAccessLevel(user),
+        }));
+    };
+
+    /**
+     * Called when a user row is clicked on
+     */
+    const onUserSelected = (user) => {
+        const userData = userMetaData.find((u) => u.Username === user.Username);
+
+        setSelectedUser(userToRoleModalFormat(userData));
+    };
+
+    /**
+     * Called when a user's data is updated
+     */
     const onUserEdited = (username, accessLevel, roles) => {
         setUserMetaData((metaData) => {
+            // Create updated access attribute
             const updatedAccess = {
                 Name: COGNITO_ATTRIBUTES.ACCESS,
                 Value: accessLevel,
             };
+
+            // Create update role attribute
             const updatedRoles = {
                 Name: COGNITO_ATTRIBUTES.ROLES,
                 Value: JSON.stringify(roles),
             };
 
+            // Clone the structure and find user
             const updatedUsers = _.cloneDeep(metaData);
             const userToUpdate = updatedUsers.find(
                 (user) => user.Username === username,
             );
 
+            // Do the update
             userToUpdate.Attributes = userToUpdate.Attributes.filter(
                 (attrib) =>
                     attrib.Name !== updatedAccess.Name &&
@@ -91,40 +141,12 @@ const AccountManagement = () => {
         });
     };
 
-    const transformData = (users) => {
-        return users.map((user) => ({
-            Username: getUsername(user),
-            Name: getName(user),
-            Email: getEmail(user),
-            Roles: getRoles(user, rolesData, selectedLang),
-            Access: getAccessLevel(user),
-        }));
-    };
-
     function generateMainUserTable() {
-        const headings = [
-            { title: 'Name', sortKey: 'Name', dataType: FIELD_TYPES.STRING },
-            { title: 'Email', sortKey: 'Email', dataType: FIELD_TYPES.STRING },
-            { title: 'Roles', sortKey: 'Roles', dataType: FIELD_TYPES.STRING },
-            {
-                title: 'Access',
-                sortKey: 'Access',
-                dataType: FIELD_TYPES.STRING,
-            },
-        ];
-
-        const rowData = [
-            { id: 'Name', dataType: FIELD_TYPES.STRING },
-            { id: 'Email', dataType: FIELD_TYPES.STRING },
-            { id: 'Roles', dataType: FIELD_TYPES.STRING },
-            { id: 'Access', dataType: FIELD_TYPES.ACCESS },
-        ];
-
         return (
             <MainTable
-                data={transformData(userMetaData)}
-                headers={headings}
-                rowData={rowData}
+                data={usersToTableFormat(userMetaData)}
+                headers={USER_TABLE_HEADERS}
+                rowData={USER_TABLE_ROW_DATA}
                 renderHeader={userTableHeaderRenderer}
                 renderTableRow={generateUserTableRowRenderer(onUserSelected)}
             />
