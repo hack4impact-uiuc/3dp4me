fbc552f8d05db4541f5d13dbd00c6fc24d5c0638
Matthew Walowski <mattwalowski@gmail.com>
2021-08-24 11:42:39 -0500

    Update file check to be more specific

--- scripts/check-sensitive-files.sh
+++ scripts/check-sensitive-files.sh
@@ -5,11 +5,9 @@ FORBIDDEN_FILES=(backend/production.env backend/development.env frontend/product
 
 for file in "${FORBIDDEN_FILES[@]}"
 do
-    if $(git diff --exit-code $file); 
+    git diff --quiet --exit-code $file
+    if [ $? -ne 0 ]; 
     then
-        continue
-    else
-        
         echo You cannot commit file $file! Run the following command:
         echo git reset HEAD -- $file
     fi
