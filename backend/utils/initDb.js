299c31ec386b4edd1d95cdc33ef87e956c70f8d7
Matthew Walowski <mattwalowski@gmail.com>
2021-09-20 21:03:03 -0500

    Run linter

--- backend/utils/initDb.js
+++ backend/utils/initDb.js
@@ -23,9 +23,7 @@ module.exports.initDB = () => {
             log.info('Connected to the DB');
             this.initModels();
         })
-        .on('error', (error) =>
-            log.error('Error connecting to the database: ', error),
-        );
+        .on('error', (error) => log.error('Error connecting to the database: ', error));
 };
 
 /**
@@ -92,30 +90,30 @@ const getStepBaseSchema = () => {
  */
 module.exports.generateFieldSchema = (field) => {
     switch (field.fieldType) {
-        case FIELDS.STRING:
-            return getStringSchema();
-        case FIELDS.MULTILINE_STRING:
-            return getStringSchema();
-        case FIELDS.NUMBER:
-            return getNumberSchema();
-        case FIELDS.DATE:
-            return getDateSchema();
-        case FIELDS.PHONE:
-            return getStringSchema();
-        case FIELDS.RADIO_BUTTON:
-            return getRadioButtonSchema(field);
-        case FIELDS.FILE:
-            return getFileSchema();
-        case FIELDS.AUDIO:
-            return getFileSchema();
-        case FIELDS.FIELD_GROUP:
-            return getFieldGroupSchema(field);
-        case FIELDS.SIGNATURE:
-            return getSignatureSchema(field);
-        case FIELDS.DIVIDER:
-            return null;
-        default:
-            throw new Error(`Unrecognized field type, ${field.type}`);
+    case FIELDS.STRING:
+        return getStringSchema();
+    case FIELDS.MULTILINE_STRING:
+        return getStringSchema();
+    case FIELDS.NUMBER:
+        return getNumberSchema();
+    case FIELDS.DATE:
+        return getDateSchema();
+    case FIELDS.PHONE:
+        return getStringSchema();
+    case FIELDS.RADIO_BUTTON:
+        return getRadioButtonSchema(field);
+    case FIELDS.FILE:
+        return getFileSchema();
+    case FIELDS.AUDIO:
+        return getFileSchema();
+    case FIELDS.FIELD_GROUP:
+        return getFieldGroupSchema(field);
+    case FIELDS.SIGNATURE:
+        return getSignatureSchema(field);
+    case FIELDS.DIVIDER:
+        return null;
+    default:
+        throw new Error(`Unrecognized field type, ${field.type}`);
     }
 };
 
@@ -135,8 +133,7 @@ const getDateSchema = () => ({
 });
 
 const getRadioButtonSchema = (fieldMetadata) => {
-    if (!fieldMetadata?.options?.length)
-        throw new Error('Radio button must have options');
+    if (!fieldMetadata?.options?.length) throw new Error('Radio button must have options');
 
     return {
         type: String,
@@ -145,8 +142,7 @@ const getRadioButtonSchema = (fieldMetadata) => {
 };
 
 const getFieldGroupSchema = (fieldMetadata) => {
-    if (!fieldMetadata?.subFields?.length)
-        throw new Error('Field groups must have sub fields');
+    if (!fieldMetadata?.subFields?.length) throw new Error('Field groups must have sub fields');
 
     return {
         type: [generateFieldsFromMetadata(fieldMetadata.subFields)],
