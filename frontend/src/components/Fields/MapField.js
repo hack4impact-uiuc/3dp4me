17557a8e02a297075f06148bd2d1012f785c8d7d
Archna-1 <archnas2@illinois.edu>
2021-09-23 22:29:01 -0500

    Define default map component

---
+++ frontend/src/components/Fields/MapField.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import Search from 'react-leaflet-search';
+import PropTypes from 'prop-types';
+
+import { COUNTRY } from '../../utils/constants';
+// <MapField position={[41.076602, 30.052495]} /> put in a container
+// Make state a prop
+// Make marker movable either by searching or dragging (and not appear if no position is passed in)
+const MapField = ({ position }) => {
+    return (
+        <Map center={position} zoom={13} scrollWheelZoom={false}>
+            <TileLayer
+                noWrap={true}
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+
+            <Marker position={position}>
+                <Popup> Popup! </Popup>
+            </Marker>
+
+            <Search
+                position="topleft"
+                inputPlaceholder="Custom placeholder"
+                // search={this.state.search}
+                showMarker={false}
+                zoom={7}
+                closeResultsOnClick={true}
+                openSearchOnLoad={false}
+                providerOptions={{
+                    region: { COUNTRY },
+                }}
+
+                // default provider OpenStreetMap
+                // provider="BingMap"
+                // providerKey="AhkdlcKxeOnNCJ1wRIPmrOXLxtEHDvuWUZhiT4GYfWgfxLthOYXs5lUMqWjQmc27"
+            >
+                {(info) => (
+                    <Marker position={info?.latLng}>
+                        {this.customPopup(info)}
+                    </Marker>
+                )}
+            </Search>
+        </Map>
+    );
+};
+
+MapField.propTypes = {
+    position: PropTypes.array,
+};
+
+export default MapField;
