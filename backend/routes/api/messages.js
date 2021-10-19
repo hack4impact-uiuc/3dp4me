39715cd358c4a2d1709e67d37fbf8232b79b1f10
Matthew Walowski <mattwalowski@gmail.com>
2021-10-19 17:53:36 -0500

    Hook up router

--- backend/routes/api/messages.js
+++ backend/routes/api/messages.js
@@ -1,5 +1,19 @@
 const express = require('express');
 
 const router = express.Router();
+const { MessagingResponse } = require('twilio').twiml;
+
+router.post('/sms', (req, res) => {
+    const twiml = new MessagingResponse();
+
+    twiml.message('The Robots are coming! Head for the hills!');
+
+    res.writeHead(200, { 'Content-Type': 'text/xml' });
+    res.end(twiml.toString());
+});
+
+router.get('/sms', (req, res) => {
+    res.send('HI FREHCN THOSaAST');
+});
 
 module.exports = router;
