61f48026ad495bd5c6d64576804b51f2481940fb
vasuchal <vasudhachalasani@gmail.com>
2021-10-26 11:43:39 -0500

    Remove unused imports

--- backend/routes/api/messages.js
+++ backend/routes/api/messages.js
@@ -1,7 +1,6 @@
 const express = require('express');
 
 const router = express.Router();
-const mongoose = require('mongoose');
 const { MessagingResponse } = require('twilio').twiml;
 
 const { models } = require('../../models/index');
