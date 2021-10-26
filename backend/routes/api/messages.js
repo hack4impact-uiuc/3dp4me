cc7626245a76c55751ccf70a72aaa4b444e0e200
vasuchal <vasudhachalasani@gmail.com>
2021-10-26 11:25:31 -0500

    Make PR changes

--- backend/routes/api/messages.js
+++ backend/routes/api/messages.js
@@ -6,51 +6,37 @@ const { MessagingResponse } = require('twilio').twiml;
 
 const { models } = require('../../models/index');
 
-const accountSid = 'ACc8a4b008fff15841eccd4636574fc564'; // Your Account SID from www.twilio.com/console
-const authToken = '0e007aea37bb8034dc3bb7ff55ce83c7'; // Your Auth Token from www.twilio.com/console
+const accountSid = process.env.ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
 
 const client = require('twilio')(accountSid, authToken);
 
 router.post('/sms', async (req, res) => {
     console.log(req);
     const phone = req.body.WaId;
-    const patient = { phoneNumber: phone };
+    const patientToCreate = { phoneNumber: phone };
 
-    let r = await mongoose.model('Patient').findOne({ phoneNumber: phone });
+    let patient = await models.Patient.findOne({ phoneNumber: phone });
 
-    if (!r) {
-        const p = new models.Patient(patient);
-        r = await p.save();
+    if (!patient) {
+        const newPatient = new models.Patient(patientToCreate);
+        patient = await newPatient.save();
     }
 
     const twiml = new MessagingResponse();
 
-    twiml.message(`${r._id}`);
+    twiml.message(`${patient._id}`);
 
     res.writeHead(200, { 'Content-Type': 'text/xml' });
     res.end(twiml.toString());
 });
 
 router.get('/sms', async (req, res) => {
-    // const phone = '8627021404';
-    // const patient = { phoneNumber: phone };
-
-    // let r = await mongoose.model('Patient').findOne({ phoneNumber: phone });
-
-    // if (!r) {
-    //     const p = new models.Patient(patient);
-    //     r = await p.save();
-    // }
-
-    // const twiml = new MessagingResponse();
-
-    // twiml.message(`${r._id}`);
-
     client.messages
         .create({
             body: 'hi',
-            to: 'whatsapp:+13098319210', // Text this number
-            from: 'whatsapp:+14155238886', // From a valid Twilio number
+            to: 'whatsapp:+13098319210',
+            from: 'whatsapp:+14155238886',
         })
         .then((message) => console.log(message.sid));
     res.send('HI FREHCN THOSaAST');
