0961b9ae7485d486d269d12aec70b80370f86647
ayogik <aykoppol@gmail.com>
2021-10-28 21:05:33 -0500

    Addressed PR comments

--- frontend/src/components/Fields/MapField.js
+++ frontend/src/components/Fields/MapField.js
@@ -6,60 +6,72 @@ import _ from 'lodash';
 
 import './MapField.scss';
 import 'react-map-gl-geocoder/dist/mapbox-gl-geocoder.css';
-import { COORDINATES, PIN_URL, MAPBOX_TOKEN } from '../../utils/constants';
+import { COORDINATES, PIN_URL, MAPBOX_TOKEN, MAP_STYLE } from '../../utils/constants';
 import { useTranslations } from '../../hooks/useTranslations';
 
 const MapField = ({
+    value,
+    initValue,
     displayName,
     isDisabled,
-    fieldId,
-    initValue,
-    value,
     onChange,
+    fieldId,
 }) => {
-    // Base Viewport, set to location saved in DB
+    /**
+     * Viewport to which the map is initialized and reset.
+     * Updated alongside the location saved in the DB.
+     */
     const initialViewport = useMemo(
         () => ({
             latitude: initValue?.latitude || COORDINATES.DEFAULT_MAP_LAT,
             longitude: initValue?.longitude || COORDINATES.DEFAULT_MAP_LONG,
-            zoom: 17, // increase zoom?
+            zoom: 17,
             transitionDuration: 100,
         }),
         [initValue],
     );
 
-    // Update current viewport as value is updated
+    const [viewport, setViewport] = useState(initialViewport);
+    const [isDragging, setIsDragging] = useState(false);
+    const mapRef = useRef();
+    const translations = useTranslations()[0];
+
+    /**
+     * Conversely, updates viewport when value changed.
+     * Important for automatically resetting the viewport when clicking Discard.
+     */
     useEffect(() => {
         const newViewport = _.cloneDeep(viewport);
-        newViewport.latitude = value?.latitude || COORDINATES.DEFAULT_MAP_LAT; // do i need the or here?
-        newViewport.longitude =
-            value?.longitude || COORDINATES.DEFAULT_MAP_LONG;
+        newViewport.latitude = value?.latitude || COORDINATES.DEFAULT_MAP_LAT;
+        newViewport.longitude = value?.longitude || COORDINATES.DEFAULT_MAP_LONG;
         setViewport(newViewport);
     }, [value]);
 
-    // Update value after the end of each drag
-    const [isDragging, setIsDragging] = useState(false);
+    /**
+     * Update value to the current viewport lat/long upon the end of a drag.
+     * Update on drag end to minimize unnecessary network updates.
+     */
     useEffect(() => {
         if (isDragging) return;
 
         const coordinates = {
-            latitude: viewport.latitude,
-            longitude: viewport.longitude,
+            latitude: viewport?.latitude,
+            longitude: viewport?.longitude,
         };
 
         onChange(fieldId, coordinates);
     }, [isDragging]);
 
-    // Initialize and update viewport (local representation)
-    const [viewport, setViewport] = useState(initialViewport);
     const updateViewport = (newView) => {
         if (isDisabled) return;
 
         setViewport(newView);
     };
 
-    const mapRef = useRef();
-
+    /**
+     * Standard Geocoder method, not written by me.
+     * Provides for updating viewport after selecting search result.
+     */
     const handleGeocoderViewportChange = (newView) => {
         const geocoderDefaultOverrides = { transitionDuration: 1000 };
 
@@ -69,15 +81,13 @@ const MapField = ({
         });
     };
 
-    const translations = useTranslations()[0];
-
     return (
         <div className="mapStyling">
             <h3>{displayName}</h3>
 
             <ReactMapGL
                 ref={mapRef}
-                mapStyle="mapbox://styles/mapbox/satellite-streets-v11"
+                mapStyle={MAP_STYLE}
                 mapboxApiAccessToken={MAPBOX_TOKEN}
                 latitude={viewport?.latitude}
                 longitude={viewport?.longitude}
@@ -94,13 +104,13 @@ const MapField = ({
                 <Marker
                     latitude={viewport?.latitude}
                     longitude={viewport?.longitude}
-                    offsetLeft={viewport.zoom * -2.5}
-                    offsetTop={viewport.zoom * -5}
+                    offsetLeft={viewport?.zoom * -2.5}
+                    offsetTop={viewport?.zoom * -5}
                 >
                     <img
                         src={PIN_URL}
-                        width={viewport.zoom * 5}
-                        height={viewport.zoom * 5}
+                        width={viewport?.zoom * 5}
+                        height={viewport?.zoom * 5}
                         alt="Location marker"
                     />
                 </Marker>
