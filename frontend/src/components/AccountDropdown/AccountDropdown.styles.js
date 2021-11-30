b5ac3be7e1c49bcd56c5c4af0b3d08cad225a9bb
Ashay Parikh <ashayparikh22@gmail.com>
2021-11-29 18:26:51 -0600

    styled button and log out

--- frontend/src/components/AccountDropdown/AccountDropdown.styles.js
+++ frontend/src/components/AccountDropdown/AccountDropdown.styles.js
@@ -4,21 +4,25 @@ export const useStyles = makeStyles({
     menuWrapper: {
         margin: '10px',
         width: 'fit-content',
+        display: 'block',
     },
     accountEmail: {
         color: 'grey',
         lineHeight: '0px',
     },
-    languageSelectorWrapper: {
-        display: 'flex',
-        flexDirection: 'row',
-        alignItems: 'center',
-    },
     languageSelector: {
-        height: '35px',
-        marginLeft: '5px',
+        height: '50px',
     },
     signOutButton: {
-        padding: '8px 0px 0px 0px',
+        height: '38px',
+        fontSize: '12px',
+        fontWeight: 'bold',
+        backgroundColor: '#ca0909',
+        color: 'white',
+        marginTop: '10px',
+        width: '100%',
+        '&:hover': {
+            background: '#ca0909',
+        },
     },
 });
