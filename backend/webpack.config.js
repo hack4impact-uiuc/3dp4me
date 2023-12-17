e91aa688cf660a9387ae9f1574ac078a4d600abc
Matthew Walowski <mattwalowski@gmail.com>
2023-12-17 09:43:10 -0600

    Update webpack config

--- backend/webpack.config.js
+++ backend/webpack.config.js
@@ -1,3 +1,20 @@
 module.exports = {
+    entry: "./src/index.ts",
     target: 'node',
+    module: {
+        rules: [
+          {
+            test: /\.tsx?$/,
+            use: 'ts-loader',
+            exclude: /node_modules/,
+          },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: __dirname + '/dist',
+    },
 };
