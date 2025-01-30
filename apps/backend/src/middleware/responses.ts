fc96a4986dfdcdf159eb7ec89e08990221352d82
Matthew Walowski <mattwalowski@gmail.com>
2025-01-29 19:11:48 -0600

    add local option to frontend

--- apps/backend/src/middleware/responses.ts
+++ apps/backend/src/middleware/responses.ts
@@ -7,6 +7,7 @@ const SRC_SELF = ["'self'", 'blob:', 'data:']
 // Describes cognito origins
 const SRC_COGNITO = [
     'https://cognito-idp.eu-north-1.amazonaws.com/',
+    'https://3dp4me-dev.auth.eu-north-1.amazoncognito.com/oauth2/token',
     'https://3dp4me-users.auth.eu-north-1.amazoncognito.com/oauth2/token',
     'https://3dp4me-prosthetic-users.auth.eu-north-1.amazoncognito.com/oauth2/token',
     'https://cognito-identity.eu-north-1.amazonaws.com/',
