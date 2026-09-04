7273d79f2cf1558475c6c6d28a9f77a5f5a46e1c
Matthew Walowski <matthew@Matthews-MacBook-Pro.local>
2026-09-03 19:28:17 -0500

    set externals

--- apps/backend/webpack.config.js
+++ apps/backend/webpack.config.js
@@ -1,6 +1,9 @@
+const nodeExternals = require('webpack-node-externals')
+
 module.exports = {
     entry: "./src/index.ts",
     target: 'node',
+    externals: [nodeExternals()],
     module: {
         rules: [
           {
