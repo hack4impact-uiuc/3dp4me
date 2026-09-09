6686e7273fbe88ebfcd9a737f88e34977cce036b
Matthew Walowski <mwalowski@clearblade.com>
2026-09-09 18:21:43 -0500

    update amplify

--- apps/frontend/src/aws/aws-exports.ts
+++ apps/frontend/src/aws/aws-exports.ts
@@ -1,16 +1,29 @@
-export const awsconfig = {
+import { ResourcesConfig } from 'aws-amplify'
+
+const callbackUrl = process.env.REACT_APP_CALLBACK_URL ?? ''
+
+export const awsconfig: ResourcesConfig = {
     Auth: {
-        identityPoolId: process.env.REACT_APP_COGNITO_IDENTITY_POOL_ID,
-        region: process.env.REACT_APP_COGNITO_REGION,
-        userPoolId: process.env.REACT_APP_COGNITO_USER_POOL_ID,
-        userPoolWebClientId: process.env.REACT_APP_COGNITO_WEB_CLIENT_ID,
-        mandatorySignIn: true,
-    },
-    oauth: {
-        domain: process.env.REACT_APP_OAUTH_DOMAIN,
-        scope: ['phone', 'email', 'openid', 'aws.cognito.signin.user.admin', 'profile'],
-        redirectSignIn: process.env.REACT_APP_CALLBACK_URL,
-        redirectSignOut: process.env.REACT_APP_CALLBACK_URL,
-        responseType: 'code',
+        Cognito: {
+            identityPoolId: process.env.REACT_APP_COGNITO_IDENTITY_POOL_ID ?? '',
+            userPoolId: process.env.REACT_APP_COGNITO_USER_POOL_ID ?? '',
+            userPoolClientId: process.env.REACT_APP_COGNITO_WEB_CLIENT_ID ?? '',
+            allowGuestAccess: false,
+            loginWith: {
+                oauth: {
+                    domain: process.env.REACT_APP_OAUTH_DOMAIN ?? '',
+                    scopes: [
+                        'phone',
+                        'email',
+                        'openid',
+                        'aws.cognito.signin.user.admin',
+                        'profile',
+                    ],
+                    redirectSignIn: [callbackUrl],
+                    redirectSignOut: [callbackUrl],
+                    responseType: 'code',
+                },
+            },
+        },
     },
 }
